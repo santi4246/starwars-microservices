40cd829caeb09cb2c2f089da7cdae49aac9a531e
Santiago L. Romero <santiago_pna@hotmail.com>
2023-06-03 21:00:35 -0300

    Initial files v1.0.0

--- characters/src/server.js
+++ characters/src/server.js
@@ -7,7 +7,6 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
