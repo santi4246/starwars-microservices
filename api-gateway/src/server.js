430a0004d2a111b2719bff608d8a5f75e0bfb50c
Santiago L. Romero <santiago_pna@hotmail.com>
2023-06-03 21:40:29 -0300

    Initial files v1.0.0

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 
 const server = express();
 server.use(morgan("dev"));
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
+server.use(express.urlencoded({ extended: true, limit: "50mb" }));
+server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
